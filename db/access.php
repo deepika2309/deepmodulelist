b5eb774f5cc92a8f85e2dde1572ee81f9cca162c
deepika2309 <deepikash6@gmail.com>
2023-03-26 16:25:35 +0530

    access page
    db/access.php

---
+++ db/access.php
@@ -0,0 +1,26 @@
+<?php
+    $capabilities = array(
+
+    'block/deepmodulelist:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/deepmodulelist:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
---
+++ lang/en/block_deepmodulelist.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Moodle test block';
+$string['deepmodulelist'] = 'Moodle test block';
+$string['deepmodulelist:addinstance'] = 'Add a new Moodle test block';
+$string['deepmodulelist:myaddinstance'] = 'Add a new Moodle test block to the My Moodle page';
